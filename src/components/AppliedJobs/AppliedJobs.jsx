6a0be2d5ededa66f4d01acf0f8b26d24e03e2a7b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-12 00:10:18 +0600

    Applied job section style modified

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,11 +6,19 @@ const AppliedJobs = () => {
   //   const [data, setData] = useState([]);
 
   const { AppliedData } = useLoaderData();
-//   console.log(AppliedData);
+  //   console.log(AppliedData);
 
   //   useEffect(() => {}, []);
   return (
     <div>
+      <div className=" text-end mr-52 mt-20 font-semibold">
+        <button className="px-3 py-2 border border-purple-700 text-purple-700 rounded-xl mr-3">
+          Remote
+        </button>
+        <button className="px-3 py-2 border border-purple-700 text-purple-700 rounded-xl">
+          On-site
+        </button>
+      </div>
       {AppliedData.map((item) => (
         <div key={item.id}>
           <div className="flex justify-between items-center mx-48 rounded-lg my-10 border border-slate-300 p-2">
@@ -19,8 +27,12 @@ const AppliedJobs = () => {
               <div>
                 <h1 className="text-xl font-semibold">{item.jobName}</h1>
                 <p className="py-2">{item.companyName}</p>
-                <button className="px-3 py-2 bg-purple-700 text-white rounded-xl mr-3">{item.jobBase}</button>
-                <button className="px-3 py-2 bg-purple-700 text-white rounded-xl">{item.jobType}</button>
+                <button className="px-3 py-2 border border-purple-700 text-purple-700 rounded-xl mr-3">
+                  {item.jobBase}
+                </button>
+                <button className="px-3 py-2 border border-purple-700 text-purple-700 rounded-xl">
+                  {item.jobType}
+                </button>
                 <div className="flex py-2">
                   <p className="mr-3">{item.location}</p>
                   <p>{item.salary}</p>
